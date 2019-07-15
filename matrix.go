a605b9098a96abc79e6ffb13d6209959263bc614
Nick Boughton <nicholasboughton@gmail.com>
2019-07-15 16:55:27 +0100

    fixed Vector coords array

--- matrix.go
+++ matrix.go
@@ -19,18 +19,18 @@ func NewMatrix(rows Int, cols Int) Matrix {
 }
 
 // Implement the sort interface
-func (s Matrix) Len() int           { return len(s) }
-func (s Matrix) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
-func (s Matrix) Less(i, j int) bool { return len(s[i]) < len(s[j]) }
+func (m Matrix) Len() int           { return len(m) }
+func (m Matrix) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
+func (m Matrix) Less(i, j int) bool { return len(m[i]) < len(m[j]) }
 
-// Find returns all sets in s that contain n
-func (s Matrix) Find(n Int) chan Set {
+// Find returns all sets in m that contain n
+func (m Matrix) Find(n Int) chan Set {
 	c := make(chan Set)
 
 	go func() {
 		defer close(c)
 
-		for _, set := range s {
+		for _, set := range m {
 			if set.Contains(n) {
 				c <- set
 			}
@@ -42,18 +42,18 @@ func (s Matrix) Find(n Int) chan Set {
 
 // MaxPathSum returns the maximum value available in a path through
 // a numerical grid, i.e a Set of sets
-func (s Matrix) MaxPathSum() Int {
-	for row := len(s) - 2; row >= 0; row-- {
-		for col := 0; col < len(s[row])-1; col++ {
-			if s[row+1][col] > s[row+1][col+1] {
-				s[row][col] += s[row+1][col]
+func (m Matrix) MaxPathSum() Int {
+	for row := len(m) - 2; row >= 0; row-- {
+		for col := 0; col < len(m[row])-1; col++ {
+			if m[row+1][col] > m[row+1][col+1] {
+				m[row][col] += m[row+1][col]
 			} else {
-				s[row][col] += s[row+1][col+1]
+				m[row][col] += m[row+1][col+1]
 			}
 		}
 	}
 
-	return s[0][0]
+	return m[0][0]
 }
 
 // Coord represents the values of coordinates within a grid
@@ -80,10 +80,10 @@ const (
 // Vector returns a ln length set of values starting at row/col extending in Direction d.
 // Vector also returns the coordinates of those values.
 // If supplied Vector will set the values to replace (in order)
-func (s Matrix) Vector(pos Coord, ln Int, d Direction, replaceWith ...Int) (Set, []Coord, error) {
+func (m Matrix) Vector(pos Coord, ln Int, d Direction, replaceWith ...Int) (Set, []Coord, error) {
 	var (
 		res  Set
-		crds = make([]Coord, ln)
+		crds []Coord
 	)
 
 	for i := Int(0); i < ln; i++ {
@@ -108,15 +108,15 @@ func (s Matrix) Vector(pos Coord, ln Int, d Direction, replaceWith ...Int) (Set,
 			crd = Coord{pos.Row - i, pos.Col - i}
 		}
 
-		if crd.Row >= Int(len(s)) || crd.Row < 0 || crd.Col >= Int(len(s[crd.Row])) || crd.Col < 0 {
-			return nil, nil, fmt.Errorf("Vector out of bounds [ROW|COL]:[%d|%d]", crd.Row, crd.Col)
+		if crd.Row >= Int(len(m)) || crd.Row < 0 || crd.Col >= Int(len(m[crd.Row])) || crd.Col < 0 {
+			return res, crds, fmt.Errorf("Vector out of bounds [ROW|COL]:[%d|%d]", crd.Row, crd.Col)
 		}
 
 		if i < Int(len(replaceWith)) {
-			s[crd.Row][crd.Col] = replaceWith[i]
+			m[crd.Row][crd.Col] = replaceWith[i]
 		}
 
-		res = append(res, s[crd.Row][crd.Col])
+		res = append(res, m[crd.Row][crd.Col])
 		crds = append(crds, crd)
 	}
 
@@ -135,8 +135,7 @@ Set{
 */
 
 // NumberSpiral creates a square grid number spiral of width size. If size is even it is incremented
-// to become odd.
-/*
+// to become odd. This function is unfinishied. Don't use it.
 func NumberSpiral(size Int) Matrix {
 	if size%2 != 0 {
 		size++
@@ -144,28 +143,52 @@ func NumberSpiral(size Int) Matrix {
 
 	var (
 		// Lets declare our bits here in a nice orderly list
-		m = NewMatrix(size, size)
-	//			vecOrder = []Direction{UP, LTR, DOWN, RTL}
-		//	vecIdx   = 0
-		//	vecLen   = Int(1)
-		r = size / 2
-		c = size / 2
+		m   = NewMatrix(size, size)
+		r   = size / 2
+		c   = size / 2
+		crd = Coord{r, c}
 	)
 
 	// LET THE FUCKERY BEGIN
 	m[r][c] = 1
-	for i := Int(1); i <= size*size; i++ {
-		if i%2 != 0 {
-			v, _, err := m.Vector(Coord{r, c}, i+1, LTR)
+	for inc := Int(1); inc <= size*size; inc++ {
+		if inc%2 != 0 {
+			v, rc, err := m.Vector(crd, inc+1, LTR, Range(m[crd.Row][crd.Col], m[crd.Row][crd.Col]+inc)...)
+			if err != nil {
+				m.Vector(crd, inc, LTR)
+				break
+			}
+			crd.Row = rc[len(rc)-1].Row
+			crd.Col = rc[len(rc)-1].Col
+			fmt.Println(v, rc)
+
+			v, rc, err = m.Vector(crd, inc+1, DOWN, Range(m[crd.Row][crd.Col], m[crd.Row][crd.Col]+inc)...)
 			if err != nil {
 				break
 			}
-		} else {
+			crd.Row = rc[len(rc)-1].Row
+			crd.Col = rc[len(rc)-1].Col
+			fmt.Println(v, rc)
 
+		} else {
+			v, rc, err := m.Vector(crd, inc+1, RTL, Range(m[crd.Row][crd.Col], m[crd.Row][crd.Col]+inc)...)
+			if err != nil {
+				break
+			}
+			crd.Row = rc[len(rc)-1].Row
+			crd.Col = rc[len(rc)-1].Col
+			fmt.Println(v, rc)
+
+			v, rc, err = m.Vector(crd, inc+1, UP, Range(m[crd.Row][crd.Col], m[crd.Row][crd.Col]+inc)...)
+			if err != nil {
+				break
+			}
+			crd.Row = rc[len(rc)-1].Row
+			crd.Col = rc[len(rc)-1].Col
+			fmt.Println(v, rc)
 		}
 
 	}
 
 	return m
 }
-*/
